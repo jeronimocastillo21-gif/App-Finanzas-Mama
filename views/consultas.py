ad919ba9744ba144b841151b35b87deb769f15dc
Jerónimo Castillo López <j.castillol2@uniandes.edu.co>
2026-03-30 19:45:48 -0500

    Fix 3

--- views/consultas.py
+++ views/consultas.py
@@ -96,6 +96,14 @@ def mostrar():
                 
         else:
             df = get_registros()
+            if len(fecha) == 2 and len(mes) == 2:
+                dat = f"{fecha}/{mes}/{anio}"
+            elif len(fecha) == 2:
+                dat = f"{fecha}/0{mes}/{anio}"
+            elif len(mes) == 2:
+                dat = f"0{fecha}/{mes}/{anio}"
+            else:
+                dat = f"0{fecha}/0{mes}/{anio}"
             df = df[df["Fecha"]==fecha]
             if tipo != "General":
                 df = df[df["Fuente"]==tipo]
