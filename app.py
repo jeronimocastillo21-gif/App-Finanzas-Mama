a38c3c83d342b512a944f6daa5a2ebf73a3e8d4f
Jerónimo Castillo López <j.castillol2@uniandes.edu.co>
2026-04-11 16:58:04 -0500

    Prueba acceso restringido

--- app.py
+++ app.py
@@ -2,7 +2,29 @@
 
 import streamlit as st
 
+from streamlit_google_auth import Authenticate
+
 # Configuración general de la página
+
+
+authenticator = Authenticate(
+    secret_credentials_path="credenciales.json",
+    cookie_name="finanzas_app",
+    cookie_key=st.secrets["COOKIE_KEY"],
+    redirect_uri="https://app-finanzas-mama.streamlit.app/"
+)
+
+authenticator.check_authentification()
+
+if not st.session_state.connected:
+    st.stop()
+
+# Restringe por correo específico
+CORREOS_PERMITIDOS = ["jeronimo.castillo21@gmail.com"]
+if st.session_state.user_info["email"] not in CORREOS_PERMITIDOS:
+    st.error("No tienes acceso a esta aplicación")
+    st.stop()
+
 st.set_page_config(
     page_title="Mis Finanzas",
     page_icon="💰",
